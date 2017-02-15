a698b033372a10ed140dc950327340ea9feaefec
menviv <earth@Menashes-MacBook-Pro.local>
2017-02-15 14:08:32 +0200

    Yalla Botting!

---
+++ messages/index.js
@@ -0,0 +1,67 @@
+/*-----------------------------------------------------------------------------
+This template demonstrates how to use Waterfalls to collect input from a user using a sequence of steps.
+For a complete walkthrough of creating this type of bot see the article at
+https://docs.botframework.com/en-us/node/builder/chat/dialogs/#waterfall
+-----------------------------------------------------------------------------*/
+"use strict";
+var builder = require("botbuilder");
+var botbuilder_azure = require("botbuilder-azure");
+var mongo = require('mongodb');
+var connString = 'mongodb://bot:bot@ds056979.mlab.com:56979/builderbot';
+var MongoClient = require('mongodb').MongoClient;
+var ObjectID = require('mongodb').ObjectID;
+var dbm;
+var collTrees;
+
+// Initialize connection once
+
+mongo.MongoClient.connect(connString, function(err, database) {
+  if(err) throw err;
+ 
+  dbm = database;
+
+  collTrees = dbm.collection('Trees');
+
+});
+
+var useEmulator = (process.env.NODE_ENV == 'development');
+
+var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
+    appId: process.env['MicrosoftAppId'],
+    appPassword: process.env['MicrosoftAppPassword'],
+    stateEndpoint: process.env['BotStateEndpoint'],
+    openIdMetadata: process.env['BotOpenIdMetadata']
+});
+
+var bot = new builder.UniversalBot(connector);
+
+bot.dialog('/', [
+    function (session) {
+        builder.Prompts.text(session, "Hello... What's your name?");
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        builder.Prompts.number(session, "Hi " + results.response + ", And...How many years have you been coding?"); 
+    },
+    function (session, results) {
+        session.userData.coding = results.response;
+        builder.Prompts.choice(session, "What language do you code Node using?", ["JavaScript", "CoffeeScript", "TypeScript"]);
+    },
+    function (session, results) {
+        session.userData.language = results.response.entity;
+        session.send("Got it... " + session.userData.name + 
+                    " you've been programming for " + session.userData.coding + 
+                    " years and use " + session.userData.language + ".");
+    }
+]);
+
+if (useEmulator) {
+    var restify = require('restify');
+    var server = restify.createServer();
+    server.listen(3978, function() {
+        console.log('test bot endpont at http://localhost:3978/api/messages');
+    });
+    server.post('/api/messages', connector.listen());    
+} else {
+    module.exports = { default: connector.listen() }
+}
