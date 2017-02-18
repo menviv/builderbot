64a87009aedbdeed62ad9c772fcfa2e9d8b7592a
Menashe Gezelkopy <menashe.gezelkopy@gmail.com>
2017-02-19 00:45:41 +0200

    initial question propmt ver 17

--- messages/index.js
+++ messages/index.js
@@ -56,19 +56,54 @@ var nAnswersCounter = parseInt("1");
 
 
 
-bot.DialogAction('/life', [
-
-        function (session) {
-
-            session.send("jfkldsj fhjdkshfkjd fhjdkshfkj"); 
-
-        }
-
-    
-]);
+var instructions = 'Welcome to the Bot to showcase the DirectLine API. Send \'Show me a hero card\' or \'Send me a BotFramework image\' to see how the DirectLine client supports custom channel data. Any other message will be echoed.';
 
 
-bot.dialog('/', [
+bot.dialog('/', function (session) {
+
+    var reply = new builder.Message()
+        .address(session.message.address);
+
+    var text = session.message.text.toLocaleLowerCase();
+    switch (text) {
+        case 'show me a hero card':
+            reply.text('Sample message with a HeroCard attachment')
+                .addAttachment(new builder.HeroCard(session)
+                    .title('Sample Hero Card')
+                    .text('Displayed in the DirectLine client'));
+            break;
+
+        case 'send me a botframework image':
+            reply.text('Sample message with an Image attachment')
+                .addAttachment({
+                    contentUrl: 'https://docs.botframework.com/en-us/images/faq-overview/botframework_overview_july.png',
+                    contentType: 'image/png',
+                    name: 'BotFrameworkOverview.png'
+                });
+
+            break;
+
+        default:
+            reply.text('You said \'' + session.message.text + '\'');
+            break;
+    }
+
+    session.send(reply);
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+bot.dialog('/sss', [
     function (session) {
         builder.Prompts.text(session, "Welcome to BuilderBot... I'm here to help you build and configure my son Bot :), but first: What's your email?");
 
