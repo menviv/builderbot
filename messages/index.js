cf4f4a07b68826b5dda0ed2ff28933f1752d2679
Menashe Gezelkopy <menashe.gezelkopy@gmail.com>
2017-02-18 16:25:42 +0200

    initial question propmt ver 9

--- messages/index.js
+++ messages/index.js
@@ -309,7 +309,7 @@ bot.dialog('/location', [
 
             session.sendTyping();
 
-            session.send(session, "Let's start by creating PROMPTS based question. My advice is to ask short and simplae questions. Example: what is your name?"); 
+            session.send("Let's start by creating PROMPTS based question. My advice is to ask short and simplae questions. Example: what is your name?"); 
 
             session.beginDialog("/pathNew_Prompts");
 
